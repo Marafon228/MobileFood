5e57fc080ed07628cb41912908b93c27058e51e6
Aaaa <d>
2021-11-18 03:38:52 +0300

    Add project files.

---
+++ MobileFood/Menedger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MobileFood
+{
+    class Menedger
+    {
+        public static Frame Main { get; set; }
+        public static Frame MainFrame { get; set; }
+    }
+}
